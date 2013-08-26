a167679317ab1ddd1f214213376315f807c27217
albertnavarro <anavarro@aidacentre.com>
2013-08-26 13:05:26 +0200

    Change-Id: I338290f30281d35b1bb558aaa6f8f8f0ce953191 Signed-off-by: albertnavarro <anavarro@aidacentre.com>

--- src/hiworld/hi.java
+++ src/hiworld/hi.java
@@ -1,12 +1,9 @@
 package hiworld;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class hi {
-	public static void main(String[] args) throws IOException {
-	
-		System.out.println ("Hola mundet");
+	public static void main(String[] args)
+	{
+			System.out.println ("Hola mundet");
 	}
 }
