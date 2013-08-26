b01aa53e1b6d9fdf02d332ba7050c28084bfe497
albertnavarro <anavarro@aidacentre.com>
2013-08-26 12:32:45 +0200

    hola mundo con petición de saludo

--- src/hiworld/hi.java
+++ src/hiworld/hi.java
@@ -1,7 +1,17 @@
 package hiworld;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class hi {
-	public static void main(String[] args) {
-		System.out.println("hola");
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader lectura = new BufferedReader(new InputStreamReader(System.in));
+		String nombre;
+		System.out.println("Ingrese su nombre: ");
+		nombre = lectura.readLine();
+		
+		System.out.println ("Hola "+nombre);
 	}
 }
